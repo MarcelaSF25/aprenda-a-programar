4541e8b011c5c0c2fdc6048160c5d274b8a9eb3b
Marcela Soares Ferreira <marcelasoares.ferreira.ms@gmail.com>
2022-08-25 21:44:03 -0300

    atividade-2

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,10 @@
 let nome 
+nome = "Marcela Soares"
 let email
+email = "marcelasoares.ferreira.ms@gmail.com"
 let profissao
+profissao = "vendedora"
 let idade
+idade = "25"
 let lugar
+lugar = "California"
