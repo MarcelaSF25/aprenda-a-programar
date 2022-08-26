14ddfca44cfb43404ab5bbcd4d53b763bbeb640d
Marcela Soares Ferreira <marcelasoares.ferreira.ms@gmail.com>
2022-08-25 21:55:04 -0300

    FInal do pratic-1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,3 +8,11 @@ let idade
 idade = "25"
 let lugar
 lugar = "California"
+console.log (nome)
+console.log(email)
+console.log (profissao)
+console.log (idade)
+console.log (lugar)
+console.log (nome) + (email)
+
+
